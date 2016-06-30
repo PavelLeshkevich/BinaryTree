061f3410f7d8df5d704cbf87910ebd7f107378a4
pavel.leshkevich <pav.leshk@gmail.com>
2016-06-30 14:52:13 +0300

    rebuilding test

--- test.cpp
+++ test.cpp
@@ -11,6 +11,8 @@ private:
 
     vector < float > nums;
 
+    vector < pair < float, int > > sol;
+
     vector < vector < float >* > tree;
 
 public:
@@ -18,12 +20,54 @@ public:
     Test(int count_element) {
 
         for (int it = 0; it < count_element; it++) {
-            int rand_num = rand() % 100;
+            int rand_num = rand() % 100000;
             if(rand() % 2 == 0) rand_num *= -1;
             nums.push_back(rand_num);
         }
-        print(generate_tree(nums));
+        TreeNode *root = generate_tree(nums);
+        sort(nums.begin(), nums.end());
+        bool ok = check(root);
+
+        if(ok) {
+            cout << "OK!!!" << endl;
+        }
+        else {
+            cout << "FAIL!!!" << endl;
+        }
     }
+
+    void go(TreeNode *cur_node, int deep) {
+        if (cur_node->left == NULL && cur_node->right == NULL) {
+            sol.push_back(make_pair(cur_node->value, deep));
+        }
+        else {
+            if(cur_node->left != NULL) {
+                go(cur_node->left, deep + 1);
+            }
+
+            if(cur_node->right != NULL) {
+                go(cur_node->right, deep + 1);
+            }
+        }
+    }
+
+    bool check(TreeNode *root) {
+        go(root, 0);
+        sort(sol.begin(), sol.end());
+        if(sol.size() != nums.size()) {
+            return false;
+        }
+        for(int i = 0; i < sol.size(); i++) {
+            if(sol[i].first != nums[i]) {
+                return false;
+            }
+            if(sol[i].second != sol[0].second) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 
     void add_vector(int deep, float value, vector < vector < float >* > *tree) {
         if (tree->size() <= deep) {
@@ -79,9 +123,12 @@ public:
 
     ~Test() {
         nums.clear();
+        sol.clear();
         tree.clear();
     }
+
 };
+
 
 
 
@@ -89,8 +136,8 @@ int main() {
 
     srand(time(NULL));
 
-    for (int test = 0; test <= 5; test++) {
-        Test *new_test = new Test(rand() % 14);
+    for (int test = 0; test <= 20; test++) {
+        Test *new_test = new Test(rand() % 100000);
         delete new_test;
     }
 
